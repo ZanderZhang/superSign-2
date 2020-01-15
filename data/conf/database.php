64c7b6b4a4613545dc1e225b200a26beb140238b
方新磊 <fangxinlei@fangxinleideMacBook-Pro.local>
2020-01-15 16:24:21 +0800

    create project

---
+++ data/conf/database.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * 配置文件
+ */
+
+return [
+    // 数据库类型
+    'type'     => 'mysql',
+    // 服务器地址
+    'hostname' => '39.106.119.227',
+    // 数据库名
+    'database' => 'super_sign',
+    // 用户名
+    'username' => 'seven',
+    // 密码
+    'password' => 'Fx829317',
+    // 端口
+    'hostport' => '3306',
+    // 数据库编码默认采用utf8
+    'charset'  => 'utf8mb4',
+    // 数据库表前缀
+    'prefix'   => 'cmf_',
+    "authcode" => 'JL5rPyAVz3wy8rKq3F',
+    //#COOKIE_PREFIX#
+];
